75973a0a2b69e0b6711112e58fd007b5b9ae39b9
rashedsumon <rashedsumon57@gmail.com>
2025-12-04 19:44:42 +0600

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,17 @@
 """
 data_loader.py
 Automatically download the PDF dataset from KaggleHub into ./data/
+Usage:
+    python data_loader.py
 """
 
 import os
 import kagglehub
 
-# Create data folder if it doesn't exist
-os.makedirs("data", exist_ok=True)
-
 # Download latest version of PDF dataset
-dataset_path = kagglehub.dataset_download("manisha717/dataset-of-pdf-files", extract=True)
+dataset_path = kagglehub.dataset_download("manisha717/dataset-of-pdf-files")
+
+# Create data folder if not exists
+os.makedirs("data", exist_ok=True)
 
 print("Path to dataset files:", dataset_path)
